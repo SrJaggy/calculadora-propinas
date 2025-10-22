7373c96b7797a612cfedd0193fd92326ccad6781
Julian Andres Giraldo <jag8210@gmail.com>
2025-10-22 14:02:38 -0500

    First commit

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,10 @@
+
+
+
+<button 
+        className=' border-2 border-teal-400 hover:bg-teal-200 p-3 text-lg  rounded-lg flex justify-between w-full'
+        
+      > 
+          <p>Nombre</p> 
+          <p className='font-black'>$1000</p>
+      </button>
